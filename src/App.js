a60938e3f54249368cf9cb43ef1e162c612be084
Deepak kumar <106810862+deepakkumar182002@users.noreply.github.com>
2024-01-22 21:21:23 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Navigation from './components/Navigation';
 
 
 function App() {
+  // console.log(process.env.REACT_APP_MY_API)
   return (
-
     <>
       {/* <div className="container"> */}
+      <h1>value:{process.env.REACT_APP_MY_API}</h1>
         <Navigation />
         <Carousel />
         
